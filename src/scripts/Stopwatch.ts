956ef4fc765b83402e06c38914472249068c2649
Matt Gaikema <mgaikema1@gmail.com>
2016-08-14 15:45:54 -0500

    Documentation

--- src/scripts/Stopwatch.ts
+++ src/scripts/Stopwatch.ts
@@ -1,14 +1,33 @@
+/**
+ * A basic Stopwatch that counts up every second,
+ * stopping at 999.
+ * 
+ * @export
+ * @class Stopwatch
+ */
 export class Stopwatch
 {
     public current_time: number;
+
     private display: HTMLElement;
 
+    /**
+     * Creates an instance of Stopwatch.
+     * 
+     * @param {HTMLElement} display
+     */
     constructor(display: HTMLElement)
     {
         this.current_time = 0;
         this.display = display;
     }
 
+    /**
+     * Add 1 to the current_time.
+     * 
+     * @private
+     * @returns {void}
+     */
     private add(): void
     {
         this.current_time++;
@@ -17,9 +36,13 @@ export class Stopwatch
         if (this.current_time == 999)
             return;
 
+        // Recursively call itself, after 1 second.
         this.time();
     }
 
+    /**
+     * Starts the Stopwatch.
+     */
     public time(): void
     {
         // https://github.com/Microsoft/TypeScript/wiki/'this'-in-TypeScript
