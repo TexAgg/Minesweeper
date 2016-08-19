d5df95c0c0deda7837ed8d61383dadfa469b0182
Matt Gaikema <mgaikema1@gmail.com>
2016-08-18 23:32:02 -0500

    Revert "suggestions link"
    This reverts commit 0ecba5ea5497cc9a9ac0e8a0b4d21680a476b37a.

--- src/templates/footer.php
+++ src/templates/footer.php
@@ -1,13 +1,6 @@
 	</div>
   <footer>
-		<div>
-			<p>
-				Bugs? Suggestions? Feedback? Submit them <a href='https://github.com/TexAgg/Minesweeper/issues'>here</a>.
-			</p>
-		</div>
-		<div>
-			<a href="http://mattgaikema.com/">Matt Gaikema</a> 2016
-		</div>
+		<a href="http://mattgaikema.com/">Matt Gaikema</a> 2016
 	</footer>
 </body>
 </html>
