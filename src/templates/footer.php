0ecba5ea5497cc9a9ac0e8a0b4d21680a476b37a
Matt Gaikema <mgaikema1@gmail.com>
2016-08-18 23:18:39 -0500

    suggestions link

--- src/templates/footer.php
+++ src/templates/footer.php
@@ -1,6 +1,13 @@
 	</div>
   <footer>
-		<a href="http://mattgaikema.com/">Matt Gaikema</a> 2016
+		<div>
+			<p>
+				Bugs? Suggestions? Feedback? Submit them <a href='https://github.com/TexAgg/Minesweeper/issues'>here</a>.
+			</p>
+		</div>
+		<div>
+			<a href="http://mattgaikema.com/">Matt Gaikema</a> 2016
+		</div>
 	</footer>
 </body>
 </html>
